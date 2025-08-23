816d3c54164bd686ce1a6edeea3aa85aa9b6538c
Niels Albarran <niels.albarran@outlook.de>
2025-08-23 10:08:58 +0200

    initial commit

---
+++ plugin.py
@@ -0,0 +1,14 @@
+class ShelterPlugin(PythonPlugin):
+    def onEnable(self):
+        pass
+
+    def onCommand(self, sender, command, label, args):
+        player_position = sender.getLocation()                      # Get the player's current position
+        dir_unit_vector = player_position.getDirection()            # Get the player's direction unit vector
+        target_position = player_position.add(dir_unit_vector)      # Calculate the target position
+
+        world = sender.getWorld()                                   # Get the world object
+        block = world.getBlockAt(target_position)               # Get the block at the target position
+        block.setType(bukkit.Material.getMaterial("GOLD_BLOCK"))               # Set the block type to gold
+
+        return True
