8d6d74052a5f2ffeb5e97e1f53477ddae224e988
Niels <niels.albarran@outlook.de>
2025-08-30 17:33:56 +0200

    re-aligning some auto-formatting from PyCharm... :(

--- shelter.py.dir/plugin.py
+++ shelter.py.dir/plugin.py
@@ -3,12 +3,12 @@ class ShelterPlugin(PythonPlugin):
         pass
 
     def onCommand(self, sender, command, label, args):
-        player_position = sender.getLocation()  # Get the player's current position
-        dir_unit_vector = player_position.getDirection()  # Get the player's direction unit vector
-        target_position = player_position.add(dir_unit_vector)  # Calculate the target position
+        player_position = sender.getLocation()                      # Get the player's current position
+        dir_unit_vector = player_position.getDirection()            # Get the player's direction unit vector
+        target_position = player_position.add(dir_unit_vector)      # Calculate the target position
 
-        world = sender.getWorld()  # Get the world object
-        block = world.getBlockAt(target_position)  # Get the block at the target position
-        block.setType(bukkit.Material.SLIME_BLOCK)  # Set the block type to gold
+        world = sender.getWorld()                                   # Get the world object
+        block = world.getBlockAt(target_position)                   # Get the block at the target position
+        block.setType(bukkit.Material.SLIME_BLOCK)                  # Set the block type to gold
 
         return True
