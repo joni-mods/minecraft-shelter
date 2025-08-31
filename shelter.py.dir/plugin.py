e05b90b78ae0dbffdac6c13c65d8402afded610e
Niels <niels.albarran@outlook.com>
2025-08-31 23:08:18 +0200

    fix: removed unused Vector import

--- shelter.py.dir/plugin.py
+++ shelter.py.dir/plugin.py
@@ -3,21 +3,21 @@ class ShelterPlugin(PythonPlugin):
         pass
 
     def onCommand(self, sender, command, label, args):
-        player_position = sender.getLocation()                              # Get the player's current position
-        dir_unit_vector = player_position.getDirection()                    # Get the player's direction unit vector
-        target_position = player_position.add(dir_unit_vector.multiply(15)) # Calculate the target position
+        player_position = sender.getLocation()
+        dir_unit_vector = player_position.getDirection()
+        target_position = player_position.add(dir_unit_vector.multiply(10))
 
-        ShelterPlugin.buildCube(self, sender, target_position,4, 6,6, bukkit.Material.STONE)
+        ShelterPlugin.buildCube(self, sender, target_position, 4, 6, 6, bukkit.Material.STONE)
         target_position.setX(target_position.getX() + 1)
         target_position.setY(target_position.getY() + 1)
         target_position.setZ(target_position.getZ() + 1)
-        ShelterPlugin.buildCube(self, sender, target_position,2, 4,4, bukkit.Material.AIR)
+        ShelterPlugin.buildCube(self, sender, target_position, 2, 4, 4, bukkit.Material.AIR)
 
         return True
 
     def buildCube(self, player, target_position, height, width, length, material):
         position = target_position.clone()
-        world = player.getWorld()                                           # Get the world object
+        world = player.getWorld()
 
         xStart = position.getX()
         yStart = position.getY()
