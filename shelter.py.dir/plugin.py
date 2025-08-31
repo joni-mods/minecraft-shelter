9a73ff23d3d79142daf7a92b626f924f5248691b
Niels <niels.albarran@outlook.de>
2025-08-31 16:50:43 +0200

    added an internal cube to create the space for the shelter

--- shelter.py.dir/plugin.py
+++ shelter.py.dir/plugin.py
@@ -3,28 +3,33 @@ class ShelterPlugin(PythonPlugin):
         pass
 
     def onCommand(self, sender, command, label, args):
-        ShelterPlugin.buildCube(self, sender, 5, 10,10, bukkit.Material.STONE)
+        player_position = sender.getLocation()                              # Get the player's current position
+        dir_unit_vector = player_position.getDirection()                    # Get the player's direction unit vector
+        target_position = player_position.add(dir_unit_vector.multiply(15)) # Calculate the target position
+
+        ShelterPlugin.buildCube(self, sender, target_position,4, 6,6, bukkit.Material.STONE)
+        target_position.setX(target_position.getX() + 1)
+        target_position.setY(target_position.getY() + 1)
+        target_position.setZ(target_position.getZ() + 1)
+        ShelterPlugin.buildCube(self, sender, target_position,2, 4,4, bukkit.Material.AIR)
 
         return True
 
-    def buildCube(self, player, height, width, length, material):
-        player_position = player.getLocation()                      # Get the player's current position
-        dir_unit_vector = player_position.getDirection()            # Get the player's direction unit vector
-        target_position = player_position.add(dir_unit_vector)      # Calculate the target position
+    def buildCube(self, player, target_position, height, width, length, material):
+        position = target_position.clone()
+        world = player.getWorld()                                           # Get the world object
 
-        world = player.getWorld()                                   # Get the world object
-
-        xStart = target_position.getX()
-        yStart = target_position.getY()
-        zStart = target_position.getZ()
+        xStart = position.getX()
+        yStart = position.getY()
+        zStart = position.getZ()
 
         for x in range(0, length):
-            target_position.setX(xStart + x)
+            position.setX(xStart + x)
             for y in range(0, height):
-                target_position.setY(yStart + y)
+                position.setY(yStart + y)
                 for z in range(0, width):
-                    target_position.setZ(zStart + z)
-                    block = world.getBlockAt(target_position)
+                    position.setZ(zStart + z)
+                    block = world.getBlockAt(position)
                     block.setType(material)
 
         return True
